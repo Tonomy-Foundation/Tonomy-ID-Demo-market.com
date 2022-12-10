d0e59566cd9f6eb7d40b1c9fc267752db263bef8
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-10 17:24:12 +0100

    added callback parameter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { App as TonomyApp} from 'tonomy-id-sdk';
 function App() {
   async function onButtonPress() {
     // change so that redirect is true by default
-    TonomyApp.onPressLogin(true);
+    TonomyApp.onPressLogin({callback: '/callback'});
   }
 
   return (
