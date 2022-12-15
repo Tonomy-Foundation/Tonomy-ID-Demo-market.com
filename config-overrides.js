21278a4a2906e7ed4a7faf76a57e4d512559bb10
Jack Tanner <jack@jackandtheblockstalk.com>
2022-12-15 16:46:12 +0100

    Added note

--- config-overrides.js
+++ config-overrides.js
@@ -11,6 +11,7 @@ module.exports = function override(config, env) {
 
     config.resolve.fallback = fallback;
 
+    // REACT_ APP_ NODE_ ENV is set in Tonomy-Integration/scripts/helpers.sh
     if (process.env.REACT_APP_NODE_ENV === 'local') {
         // https://github.com/risenforces/craco-alias/issues/24#issuecomment-801176626
         console.log('Local environment detected, using local Tonomy-ID-SDK');
